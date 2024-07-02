2c11371c3e37e5991891338ae43d1a65df6270de
Lenin Canio <lcanio@csumb.edu>
2024-07-02 14:12:15 -0700

    Updated Enrollment entity

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -1,6 +1,8 @@
 package com.cst438.domain;
 
 import jakarta.persistence.*;
+import java.sql.Date;
+import java.util.List;
 
 @Entity
 public class Enrollment {
@@ -11,7 +13,40 @@ public class Enrollment {
 	
 	// TODO complete this class
     // add additional attribute for grade
+    private String grade;
+
     // create relationship between enrollment and user entities
+    @ManyToOne
+    @JoinColumn(name="id", nullable = false)
+    private User user;
     // create relationship between enrollment and section entities
+    @ManyToOne
+    @JoinColumn(name="section_no", nullable = false)
+    private Section section;
+
     // add getter/setter methods
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
