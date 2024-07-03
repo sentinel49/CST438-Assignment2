9dfbddaca28a8eeff17fd3a2f506b3228a0684ba
BdenisCSUMB <126608445+BdenisCSUMB@users.noreply.github.com>
2024-07-02 19:00:32 -0700

    Finished Grade Entity: added additional attribute for score, added relationships to assignment and enrollment, added getter/setter methods.

--- src/main/java/com/cst438/domain/Grade.java
+++ src/main/java/com/cst438/domain/Grade.java
@@ -8,10 +8,50 @@ public class Grade {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="grade_id")
     private int gradeId;
- 
-    // TODO complete this class
+
     // add additional attribute for score
+    @Column(name ="score", nullable = true)
+    private Integer score;
+
     // add relationship between grade and assignment entities
+    @ManyToOne
+    @JoinColumn(name = "assignment_id", nullable = false)
+    private Assignment assignment;
     // add relationship between grade and enrollment entities
+    @ManyToOne
+    @JoinColumn(name = "enrollment_id", nullable = false)
+    private Enrollment enrollment;
+
     // add getter/setter methods
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    public Enrollment getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Enrollment enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
 }
