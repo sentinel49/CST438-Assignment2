e25ca8103f4470a39b3581ad72e71c91756a9ae1
BdenisCSUMB <126608445+BdenisCSUMB@users.noreply.github.com>
2024-07-10 21:51:27 -0700

    updated all termid to 10 so get request would return something

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,16 +24,16 @@ insert into course values
 ('cst499', 'Capstone', 4);
 
 insert into section(section_no, course_id, sec_id, term_id, building, room, times, instructor_email ) values
-(1, 'cst338', 1, 8, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
-(2, 'cst338', 2, 8, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
-(3, 'cst363', 1, 8, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu'),
-(4, 'cst363', 2, 8, '052', '102', 'M W 2:00-3:50', 'dwisneski@csumb.edu'),
-(5, 'cst438', 1, 8, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu'),
-(6, 'cst338', 1, 9, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
-(7, 'cst338', 2, 9, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
-(8, 'cst363', 1, 9, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu'),
-(9, 'cst363', 2, 9, '052', '102', 'M W 2:00-3:50', 'dwisneski@csumb.edu'),
-(10, 'cst438', 1, 9, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu');
+(1, 'cst338', 1, 10, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
+(2, 'cst338', 2, 10, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
+(3, 'cst363', 1, 10, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu'),
+(4, 'cst363', 2, 10, '052', '102', 'M W 2:00-3:50', 'dwisneski@csumb.edu'),
+(5, 'cst438', 1, 10, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu'),
+(6, 'cst338', 1, 10, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
+(7, 'cst338', 2, 10, '052', '100', 'M W 10:00-11:50', 'jgross@csumb.edu'),
+(8, 'cst363', 1, 10, '052', '104', 'M W 10:00-11:50', 'dwisneski@csumb.edu'),
+(9, 'cst363', 2, 10, '052', '102', 'M W 2:00-3:50', 'dwisneski@csumb.edu'),
+(10, 'cst438', 1, 10, '052', '222', 'T Th 12:00-1:50', 'dwisneski@csumb.edu');
 
 insert into enrollment (enrollment_id, grade, section_no, user_id ) values
 (1, 'A', 1, 3),
